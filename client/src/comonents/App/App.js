f5a1a8641feffc43dc540f7b70855184a45ea7a9
Максимилиан Андриянов <fazercaso@gmail.com>
2022-07-21 18:04:33 +0300

    allForStart

---
+++ client/src/comonents/App/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
