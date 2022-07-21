f795c8147f61e45ee6cdb7c4ea142ba48d84fd19
Максимилиан Андриянов <fazercaso@gmail.com>
2022-07-21 21:00:21 +0300

    chtoto

--- client/src/comonents/NavBar/NavBar.jsx
+++ client/src/comonents/NavBar/NavBar.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { Button, Link, ButtonGroup } from '@geist-ui/core'
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Button,  ButtonGroup } from '@geist-ui/core'
 
 function NavBar() {
+    const {user} = useSelector(state=> state.user)
     return (
+        <div>
+        {/* {user.id 
+        ? 
+        <>
+            <ButtonGroup justify="center" type="warning" ghost>
+            <Button><Link to="/home">Игра</Link></Button>
+            <Button><Link to="/sign/up">Профиль</Link></Button >
+            <Button><Link to="/sign/out">Выйти</Link></Button >
+            </ButtonGroup >
+        </>
+        :
+        <>
+            <ButtonGroup justify="center" type="warning" ghost>
+            <Button><Link to="/sign/in">Логин-Гогин</Link></Button>
+            <Button><Link to="/sign/up">Рега</Link></Button >
+            </ButtonGroup >
+        </>
+        } */}
+        </div>
 
-        <ButtonGroup type="warning" ghost>
-            <Button><Link to="/login">Login </Link></Button>
-            <Button><Link to="/info">Info</Link></Button >
-            <Button><Link to="/restorans">Restorans</Link></Button >
-        </ButtonGroup >
+        // <ButtonGroup justify="center" type="warning" ghost>
+        //     <Button><Link to="/sign/in">Login </Link></Button>
+        //     <Button><Link to="/sign/up">Info</Link></Button >
+        //     <Button><Link to="/restorans">Restorans</Link></Button >
+        // </ButtonGroup >
 
     );
 }
