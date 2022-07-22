d095d7578b178969119b98a50e46030c364d6263
PolinaMakarenko <pollinamakarenko@gmail.com>
2022-07-22 15:46:49 +0300

    fa

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -3,6 +3,7 @@ import { SET_USER } from "../actionTypes/userAT";
 const userReduser = (state = { user:[] }, action) => {
   switch (action.type) {
     case SET_USER:
+      console.log(action.payload);
       return { ...state, user: action.payload };
 
     default:
